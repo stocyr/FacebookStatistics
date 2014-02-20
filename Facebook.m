8b8c8608f8a9f00326c5606dd076000dc3f3cf01
Cyril Stoller <cyril.stoller@gmail.com>
2014-02-20 15:14:30 +0100

    final version with additional faked post today

--- Facebook.m
+++ Facebook.m
@@ -22,7 +22,7 @@ first_date = d(1);
 last_date = d(end);
 
 %% prepare linear axis and count number of posts / day
-x = (first_date-smoothing:1:last_date+smoothing)';
+x = datenum((first_date-10*smoothing:1:last_date+5*smoothing)');
 y = zeros(length(x),1);
 
 for i = 1:length(x)
@@ -30,17 +30,41 @@ for i = 1:length(x)
 end
 
 %% filter the y values
-windowWidth = 1;  % <<<===== CHANGE SMOOTHING VALUE HERE!!!!
+windowWidth = 150;  % <<<===== CHANGE SMOOTHING VALUE HERE!!!!
 halfWindow = windowWidth/2;
 gaussFilter = gausswin(windowWidth);
 gaussFilter = gaussFilter / sum(gaussFilter);   % Normalize
 smoothY = conv(y, gaussFilter);
 y2 = smoothY(ceil(halfWindow):end-floor(halfWindow));
 
+% plot semester end / begin
+hold all;
+sem1 = [datenum('19-sep-2011') datenum('20-jan-2012')];
+sem2 = [datenum('15-feb-2012') datenum('15-jun-2012')];
+sem3 = [datenum('19-sep-2012') datenum('20-jan-2013')];
+sem4 = [datenum('15-feb-2013') datenum('15-jun-2013')];
+sem5 = [datenum('10-sep-2013') datenum('20-jan-2014')];
+sem6 = [datenum('15-feb-2014') datenum('15-jun-2014')];
+alpha(0.5);
+semester = fill([sem1(1) sem1(2) sem1(2) sem1(1)], [0 0 10 10], 'r');
+fill([sem2(1) sem2(2) sem2(2) sem2(1)], [0 0 10 10], 'r');
+fill([sem3(1) sem3(2) sem3(2) sem3(1)], [0 0 10 10], 'r');
+fill([sem4(1) sem4(2) sem4(2) sem4(1)], [0 0 10 10], 'r');
+fill([sem5(1) sem5(2) sem5(2) sem5(1)], [0 0 10 10], 'r');
+fill([sem6(1) sem6(2) sem6(2) sem6(1)], [0 0 10 10], 'r');
+
 % plot da shit
-plot(x, y2, 'LineWidth', 2, 'LineSmoothing', 'on');
+posts = plot(x, y2, 'LineWidth', 2, 'linesmoothing', 'on');
+
+% cosmetics
+xlim([datenum('01-aug-2011') datenum('01-apr-2014')]);
+ylim([0 max(y2)*1.01]);
+tick_locations = datenum(2011,8:1:40,1);
+set(gca,'XTick',tick_locations);
 ylabel('posts / day'); grid;
-datetick('x', 'dd. mmmm yyyy', 'keepticks');
+datetick('x', 'mmm yyyy', 'keeplimits', 'keepticks');
+xticklabel_rotate;
+legend([posts, semester], {'posts / day', 'semester period'});
 
 % save as png
 %filename = ['Plots/gauss_' int2str(windowWidth) '.png'];
